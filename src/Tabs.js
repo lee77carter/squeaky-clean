654678b691efe95089a27ec45d77e35f8a7395df
lee_c <gitleelee@gmail.com>
2023-12-16 23:58:30 -0800

    Clean-up

---
+++ src/Tabs.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Tabs = () => {
+  return (
+    <section className="tabs">
+      <div></div>
+      <div></div>
+    </section>
+  );
+};
+
+export default Tabs;
---
+++ src/services.js
@@ -0,0 +1,20 @@
+const services = [
+    {
+      name: "Self-service",
+      description:
+        "Our clean and modern facility is equipped with top-of-the-line washers and dryers, giving you everything you need to get your laundry done quickly and efficiently. Whether you're washing your clothes, bedding, or towels, our machines can handle it all."
+    },
+    {
+      name: "Fluff & Fold",
+      description:
+        "Say goodbye to laundry day with our Fluff & Fold service! We're here to take the hassle out of doing laundry so you can focus on what matters most. Our full-service laundry solution includes washing, drying, and folding your clothes using high-quality detergents and softeners."
+    },
+    {
+      name: "Dry Cleaning",
+      description:
+        "We specialize in cleaning delicate fabrics and textiles that may be damaged by traditional washing methods, such as wool, silk, and leather. Our cleaning process involves using special solvents and techniques that remove stains and odors without damaging the fabric or color."
+    }
+  ];
+  
+  export default services;
+  
